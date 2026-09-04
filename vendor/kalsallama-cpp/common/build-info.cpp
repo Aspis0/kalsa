09bdfe949403db7cd793da9471c5a78c50d62966
Marco Gualtieri <66642624+Aspis0@users.noreply.github.com>
2026-09-03 20:49:52 -0400

    chore(engine): bump kalsallama pin a0cabca6b -> 12f6d9b6d (3 upstream OpenCL picks, QDC cell #53 validated)

--- vendor/kalsallama-cpp/common/build-info.cpp
+++ vendor/kalsallama-cpp/common/build-info.cpp
@@ -3,8 +3,8 @@
 #include <cstdio>
 #include <string>
 
-int LLAMA_BUILD_NUMBER = 10033;
-char const * LLAMA_COMMIT = "a0cabca";
+int LLAMA_BUILD_NUMBER = 10036;
+char const * LLAMA_COMMIT = "12f6d9b";
 char const * LLAMA_COMPILER = "unknown";
 char const * LLAMA_BUILD_TARGET = "unknown";
 
