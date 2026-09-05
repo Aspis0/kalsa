c5c55a4897f96398c707987e65afce97274e08d9
Marco Gualtieri <66642624+Aspis0@users.noreply.github.com>
2026-09-04 20:21:50 -0400

    fix(android): correct ReactPackage import in GovernorBatteryModule

--- native/GovernorBatteryModule.kt
+++ native/GovernorBatteryModule.kt
@@ -9,7 +9,7 @@ import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactPackage
+import com.facebook.react.ReactPackage
 import com.facebook.react.uimanager.ViewManager
 
 class GovernorBatteryModule(context: ReactApplicationContext) :
